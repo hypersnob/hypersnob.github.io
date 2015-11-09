2ae4868ce6529e251a6dd22e8b60eaf772253a1e
oleg zayarniy <olegza@gmail.com>
2015-11-09 22:21:35 +0100

    analytics & stuff

--- js/main.js
+++ js/main.js
@@ -3,10 +3,10 @@ $(window).scroll(function(){
 		windowHeight = $(window).height(),
 		offsetEl = $('.info').offset();
 
-	$( '.intro' ).css("opacity", 1 - windowScrollTop / 300);
+	$( '.intro' ).css('opacity', 1 - windowScrollTop / 300);
 
 	function info_animation() {
-		$( ".info .info__block" ).each(function(index) {
+		$( '.info .info-block' ).each(function(index) {
 			var that = this;
 			setTimeout(function() {
 				$(that).addClass( 'visible' );
@@ -19,28 +19,12 @@ $(window).scroll(function(){
 	}
 });
 
-// Smoos scrollTo
-// from https://css-tricks.com/examples/SmoothPageScroll/
-$(function() {
-	$('a[href*=#]:not([href=#])').click(function() {
-		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-			var target = $(this.hash);
-			target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-		if (target.length) {
-			$('html,body').animate({
-				scrollTop: target.offset().top
-			}, 800);
-			return false;
-			}
-		}
-	});
+// Scroll to Projects
+$('.intro').click(function() {
+	$('html,body').animate({
+		scrollTop: $('.projects').offset().top
+	}, 800);
 });
-
-// $( '.project-preview' ).hover(function(){
-// 	$('.project-preview').not(this).each(function(){
-// 		$(this).toggleClass('fade');
-// 	});
-// });
 
 // Project Modals
 window.REMODAL_GLOBALS = {
