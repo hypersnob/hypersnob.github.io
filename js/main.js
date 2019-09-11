8e02c14f2197fd325b9da52d89ac277b500d98d0
Oleg Zayarniy <olegza@gmail.com>
2019-09-11 15:33:16 +0200

    killing dependencies

--- js/main.js
+++ js/main.js
@@ -1,36 +1,60 @@
-$(window).scroll(function(){
-	var windowScrollTop = $(window).scrollTop(),
-		windowHeight = $(window).height(),
-		offsetEl = $('.info').offset();
+'use strict'
+document.onclick = function(e) {
+	let target = e.target;
 
-	$( '.intro' ).css('opacity', 1 - windowScrollTop / 300);
-
-	function info_animation() {
-		$( '.info .info-block' ).each(function(index) {
-			var that = this;
-			setTimeout(function() {
-				$(that).addClass( 'visible' );
-			}, 500 * index);
+	if (target.closest('[data-modal-target]')) {
+		const modals = document.querySelectorAll('[data-modal-id]')
+		for (let modal of modals) {
+			if (modal.dataset.modalId == target.closest('[data-modal-target]').dataset.modalTarget) {
+				modal.classList.add('opening')
+				setTimeout(
+					() => {
+						modal.classList.add('open')
+						modal.classList.remove('opening')
+						document.body.classList.add('noscroll')
+					}, 500
+					) 
+				}
+			}
+		return false;
+	} else if (target.hasAttribute('data-modal-close')) {
+		target.closest('[data-modal-id]').classList.add('closing')
+		setTimeout(
+			() => {
+				target.closest('[data-modal-id]').classList.remove('open')
+				target.closest('[data-modal-id]').classList.remove('closing')
+				document.body.classList.remove('noscroll')
+			}, 500
+			)
+		return false;
+	} else if (target.closest('.intro')) {
+			document.querySelector('.projects').scrollIntoView({
+			behavior: 'smooth' 
 		});
+	} else {
+		return
+	}
+}
+
+window.addEventListener('scroll', function() {
+	let windowOffset = pageYOffset,
+	windowHeight = document.documentElement.clientHeight,
+	intro = document.querySelector('.intro'),
+	opc = 1 - windowOffset/300,
+	offsetEl = document.querySelector('.info').getBoundingClientRect().top;
+	
+	intro.style.opacity = opc >= 0 ? opc.toFixed(1) : 0
+
+	if( offsetEl <= windowHeight/2 ){
+		info_animation()
 	}
 	
-	if(( offsetEl.top - windowScrollTop ) <= ( windowHeight/2 )){
-		info_animation();
-	}
 });
 
-// Scroll to Projects
-$('.intro').click(function() {
-	$('html,body').animate({
-		scrollTop: $('.projects').offset().top
-	}, 800);
-});
+function info_animation() {
+	let infos = document.querySelectorAll('.info-block')
 
-// Project Modals
-window.REMODAL_GLOBALS = {
-	NAMESPACE: 'over',
-	DEFAULTS: {
-		hashTracking: false,
-		closeOnOutsideClick: false
-	}
-};
+	infos.forEach(function(info, index) {
+		setTimeout( () => info.classList.add( 'visible' ) , 500 * index)
+	})
+}
